a2d91e1899704b0fa45a89f4ab774c5ed96a0966
enigjoka <enigjoka0515@gmail.com>
2025-04-26 11:57:25 +0200

    update service 3

--- src/main/java/org/mmeci/entity/Client.java
+++ src/main/java/org/mmeci/entity/Client.java
@@ -1,28 +1,4 @@
 package org.mmeci.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-import java.util.UUID;
-
-@Entity(name = "clients")
-@Data
-@RequiredArgsConstructor
 public class Client {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String name;
-    private String lastname;
-    private String email;
-
-    public Client(String name, String lastname, String email) {
-        this.name = name;
-        this.lastname = lastname;
-        this.email = email;
-    }
 }
