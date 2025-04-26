afed05fd1f3bfa3cb89c59ae04c82e0034d201dd
danjelamuka <danjelaamuka@gmail.com>
2025-04-26 12:57:17 +0200

    Update client v2

--- src/main/java/org/mmeci/entity/Client.java
+++ src/main/java/org/mmeci/entity/Client.java
@@ -1,4 +1,28 @@
 package org.mmeci.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity(name = "clients")
+@Data
+@RequiredArgsConstructor
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+
+    public Client(Long id, String name, String lastname, String email) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+    }
 }
