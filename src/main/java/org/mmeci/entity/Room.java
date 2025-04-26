6eb8c771e746908df91760d4cde94a98c85af05e
lusilablogu <lusilablogu@gmail.com>
2025-04-26 13:28:37 +0200

    Update Room

--- src/main/java/org/mmeci/entity/Room.java
+++ src/main/java/org/mmeci/entity/Room.java
@@ -1,5 +1,24 @@
 package org.mmeci.entity;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "rooms")
+
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    private TypeOfRoom typeOfRoom;
+
+    private double price;
+    private boolean available;
+
+    @ManyToOne
+    private Hotel hotel;
 
 }
